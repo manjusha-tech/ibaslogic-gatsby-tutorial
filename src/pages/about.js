23232cc5b33c8d21679074d27ac330cf74202e40
manjusha-tech <139439820+manjusha-tech@users.noreply.github.com>
2023-08-16 14:10:10 +0530

    Add files via upload

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,15 @@
 import React from "react";
+import Layout from "../components/layout";
+import Metadata from "../components/metadata";
 
 const About = () => {
   return (
     <div>
-      <h1>About Page</h1>
-      <p>Ibas is a web developer, teacher and a creative writer.</p>
+      <Layout>
+        <Metadata title="About Us" description="This is my about page" />
+        <h1>About Page</h1>
+        <p>Ibas is a web developer, teacher and a creative writer.</p>
+      </Layout>
     </div>
   );
 };
