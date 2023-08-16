444f1dd1dfc2a5b905c37e8a11ec307a39c3829c
midhun-phenomtec <110382543+midhun-phenomtec@users.noreply.github.com>
2023-08-16 11:48:55 +0530

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,5 +8,36 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  plugins: [],
-}
+  siteMetadata: {
+    title: "Ibas Majid",
+    description: "I teach JavaScript, React, GraphQL and Gatsby",
+    author: "Ibaslogic",
+  },
+  plugins: [
+    "gatsby-plugin-sass",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "src",
+        path: `${__dirname}/src/`,
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [
+          {
+            resolve: "gatsby-remark-images",
+            options: {
+              maxWidth: 750,
+              linkImagesToOriginal: false,
+            },
+          },
+        ],
+      },
+    },
+  ],
+};
