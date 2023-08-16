43564284c157f688a3a759344eb56a5697eb154d
manjusha-tech <139439820+manjusha-tech@users.noreply.github.com>
2023-08-16 14:10:55 +0530

    Add files via upload

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,14 @@
 import React from "react";
+import Layout from "../components/layout";
+import Metadata from "../components/metadata";
 
 const Contact = () => {
   return (
-    <div>
+    <Layout>
+      <Metadata title="Contact Us" description="This is my contact page" />
       <h1>Contact Page</h1>
       <p>You can find Ibas on Twitter via @ibaslogic</p>
-    </div>
+    </Layout>
   );
 };
 
